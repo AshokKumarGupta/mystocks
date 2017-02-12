9d83e7a6780ae141281acad97664e51a8fe96890
Ashok Gupta <ashokkumarweb@gmail.com>
2017-02-12 23:29:06 +0530

    added response

--- src/app/finance.service.ts
+++ src/app/finance.service.ts
@@ -3,16 +3,17 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
+
 export class FinanceService {
   sUrl:any;
-  constructor (
-    private http: Http
-  ) {}
+  constructor (private http: Http) {
+
+  }
 
   getStocks(ticker) {
   	this.sUrl = "http://finance.google.com/finance/info?client=ig&q=";
     return this.http.get(this.sUrl+ticker)
-    .map((res:Response) => res.json());
+    .map((res) => res.json());
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,21 +13,40 @@ export class HomePage {
   stocks: any[];
   newstock:string; 
   showField:boolean;
+  myStockList: myStockList;
+  httpresponse:{};
+  errorMessage: string;
   
 
   constructor(public navCtrl: NavController, private financeService: FinanceService) {
-  	  	this.stocks = [{name: "HDFC",	price: 500.2, upordown:"up"}, {name: "ICICI",	price: 1410.2, upordown:"up"},{name: "SBI",	price: 800.2, upordown:"up"}]
+  	  	this.stocks = [{
+	  	  		name: "HDFC",	
+	  	  		price: 500.2, 
+	  	  		upordown:"up"
+  	  		}, 
+  	  		{
+	  	  		name: "ICICI",	
+	  	  		price: 1410.2, 
+	  	  		upordown:"up"
+  	  		},
+  	  		{
+	  	  		name: "SBI",	
+	  	  		price: 800.2, 
+	  	  		upordown:"up"
+  	  		}]
   }
 
   toggleSearchVisibility(sname){
     this.showField = !this.showField;
   }
 
+
+
   addNewStock(sname){
-	this.stocks.push({name:sname,price:1,upordown:"up"})
-	this.financeService.getStocks('HDFC').subscribe(data => {
-		console.log(JSON.parse(data));
-	});
+	// this.stocks.push({name:sname,price:1,upordown:"up"})
+	this.financeService.getStocks(sname).subscribe(data => this.myStockList = JSON.stringify(data); console.log(this.myStockList),
+                       error =>  this.errorMessage = <any>error);
+
   }
 
   removeStock(index){
@@ -35,5 +54,24 @@ export class HomePage {
   }
   
 
+}
+
+interface myStockList {
+	c:string
+	"c_fix":string
+	ccol:string
+	cp:string
+	"cp_fix":string
+	e:string
+	id:string
+	l:string
+	"l_cur":string
+	"l_fix":string
+	lt:string
+	"lt_dts":string
+	ltt:string
+	"pcls_fix":string
+	s:string
+	t:string
 }
 
